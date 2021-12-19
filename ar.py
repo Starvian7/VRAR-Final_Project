3b5d575e0e401b32608e3650c52c604e3fe70f73
QiYu Wu <t107590061@ntut.org.tw>
2021-12-19 17:34:15 +0800

    initial push

---
+++ ar.py
@@ -0,0 +1,86 @@
+# v0 - calculates the homography from scratch at each step
+import cv2
+import numpy as np
+import math
+from object_module import *
+import sys
+import aruco_module as aruco 
+from my_constants import *
+from utils import get_extended_RT
+
+def init(object_directory, object_textures, object_size):
+		obj = three_d_object(object_directory, object_textures)
+		marker_colored = cv2.imread('data/m1.png')
+		assert marker_colored is not None, "Could not find the aruco marker image file"
+		#accounts for lateral inversion caused by the webcam
+		marker_colored = cv2.flip(marker_colored, 1)
+
+		marker_colored =  cv2.resize(marker_colored, (480,480), interpolation = cv2.INTER_CUBIC )
+		marker = cv2.cvtColor(marker_colored, cv2.COLOR_BGR2GRAY)
+
+		print("trying to access the webcam")
+		cv2.namedWindow("webcam")
+		vc = cv2.VideoCapture(0)
+		assert vc.isOpened(), "couldn't access the webcam"
+		
+		h,w = marker.shape
+		#considering all 4 rotations
+		marker_sig1 = aruco.get_bit_sig(marker, np.array([[0,0],[0,w], [h,w], [h,0]]).reshape(4,1,2))
+		marker_sig2 = aruco.get_bit_sig(marker, np.array([[0,w], [h,w], [h,0], [0,0]]).reshape(4,1,2))
+		marker_sig3 = aruco.get_bit_sig(marker, np.array([[h,w],[h,0], [0,0], [0,w]]).reshape(4,1,2))
+		marker_sig4 = aruco.get_bit_sig(marker, np.array([[h,0],[0,0], [0,w], [h,w]]).reshape(4,1,2))
+
+		sigs = [marker_sig1, marker_sig2, marker_sig3, marker_sig4]
+
+		rval, frame = vc.read()
+		assert rval, "couldn't access the webcam"
+		h2, w2,  _ = frame.shape
+
+		h_canvas = max(h, h2)
+		w_canvas = w + w2
+
+		while rval:
+			rval, frame = vc.read() #fetch frame from webcam
+			key = cv2.waitKey(20) 
+			if key == 27: # Escape key to exit the program
+				break
+
+			canvas = np.zeros((h_canvas, w_canvas, 3), np.uint8) #final display
+			canvas[:h, :w, :] = marker_colored #marker for reference
+
+			success, H = aruco.find_homography_aruco(frame, marker, sigs)
+			# success = False
+			if not success:
+				# print('homograpy est failed')
+				canvas[:h2 , w: , :] = np.flip(frame, axis = 1)
+				cv2.imshow("webcam", canvas )
+				continue
+
+			R_T = get_extended_RT(A, H)
+			transformation = A.dot(R_T) 
+			
+			augmented = np.flip(augment(frame, obj, transformation, marker, object_size), axis = 1) #flipped for better control
+			canvas[:h2 , w: , :] = augmented
+			cv2.imshow("webcam", canvas)
+
+
+def main():
+	print("Select the number of object model:")
+	print("1. Fox")
+	print("2. Car")
+	print("3. Android Robot")
+
+	model_number = int(input("Input of object model: "))
+
+	if model_number == 1:
+		init('data/3d_objects/low-poly-fox-by-pixelmannen.obj', 'data/3d_objects/texture.png', 4)
+	elif model_number == 2:
+		init('data/3d_objects/camero-2010-low-poly.obj', 'data/3d_objects/camero-map.png', 100)
+	elif model_number == 3:
+		init('data/3d_objects/andy.obj', 'data/3d_objects/andy.png', 1000)
+	else:
+		print("Please select number 1~3 only")
+		main()
+
+if __name__ == '__main__':
+	main()
---
+++ object_module.py
@@ -0,0 +1,136 @@
+# some functions for creation and rendering of 3d objects were borrowed/inspired from 
+# Juan Gallostra Acín's repository - https://github.com/juangallostra/augmented-reality 
+
+'''
+Copyright (c) 2018 Juan Gallostra Acín
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+#contains code to the .obj file and augment the object
+
+import cv2
+import numpy as np
+
+
+def augment(img, obj, projection, template, scale = 4):
+    # takes the captureed image, object to augment, and transformation matrix  
+    #adjust scale to make the object smaller or bigger, 4 works for the fox
+
+    h, w = template.shape
+    vertices = obj.vertices
+    img = np.ascontiguousarray(img, dtype=np.uint8)
+
+    #blacking out the aruco marker
+    a = np.array([[0,0,0], [w, 0, 0],  [w,h,0],  [0, h, 0]], np.float64 )
+    imgpts = np.int32(cv2.perspectiveTransform(a.reshape(-1, 1, 3), projection))
+    cv2.fillConvexPoly(img, imgpts, (0,0,0))
+
+    #projecting the faces to pixel coords and then drawing
+    for face in obj.faces:
+        #a face is a list [face_vertices, face_tex_coords, face_col]
+        face_vertices = face[0]
+        points = np.array([vertices[vertex - 1] for vertex in face_vertices]) #-1 because of the shifted numbering
+        points = scale*points
+        points = np.array([[p[2] + w/2, p[0] + h/2, p[1]] for p in points]) #shifted to centre 
+        dst = cv2.perspectiveTransform(points.reshape(-1, 1, 3), projection)#transforming to pixel coords
+        imgpts = np.int32(dst)
+        cv2.fillConvexPoly(img, imgpts, face[-1])
+        
+    return img
+
+class three_d_object:
+    def __init__(self, filename_obj, filename_texture, color_fixed = False):
+        self.texture = cv2.imread(filename_texture)
+        self.vertices = []
+        self.faces = []
+        #each face is a list of [lis_vertices, lis_texcoords, color]
+        self.texcoords = []
+
+        for line in open(filename_obj, "r"):
+            if line.startswith('#'): 
+                #it's a comment, ignore 
+                continue
+
+            values = line.split()
+            if not values:
+                continue
+            
+            if values[0] == 'v':
+                #vertex description (x, y, z)
+                v = [float(a) for a in values[1:4] ]
+                self.vertices.append(v)
+
+            elif values[0] == 'vt':
+                #texture coordinate (u, v)
+                self.texcoords.append([float(a) for a in values[1:3] ])
+
+            elif values[0] == 'f':
+                #face description 
+                face_vertices = []
+                face_texcoords = []
+                for v in values[1:]:
+                    w = v.split('/')
+                    face_vertices.append(int(w[0]))
+                    if len(w) >= 2 and len(w[1]) > 0:
+                        face_texcoords.append(int(w[1]))
+                    else:
+                        color_fixed = True
+                        face_texcoords.append(0)
+                self.faces.append([face_vertices, face_texcoords])
+
+
+        for f in self.faces:
+            if not color_fixed:
+                f.append(three_d_object.decide_face_color(f[-1], self.texture, self.texcoords))
+            else:
+                f.append((50, 50, 50)) #default color
+
+        # cv2.imwrite('texture_marked.png', self.texture)
+
+    def decide_face_color(hex_color, texture, textures):
+        #doesnt use proper texture
+        #takes the color at the mean of the texture coords
+
+        h, w, _ = texture.shape
+        col = np.zeros(3)
+        coord = np.zeros(2)
+        all_us = []
+        all_vs = []
+
+        for i in hex_color:
+            t = textures[i - 1]
+            coord = np.array([t[0], t[1]])
+            u , v = int(w*(t[0]) - 0.0001), int(h*(1-t[1])- 0.0001)
+            all_us.append(u)
+            all_vs.append(v)
+
+        u = int(sum(all_us)/len(all_us))
+        v = int(sum(all_vs)/len(all_vs))
+
+        # all_us.append(all_us[0])
+        # all_vs.append(all_vs[0])
+        # for i in range(len(all_us) - 1):
+        #     texture = cv2.line(texture, (all_us[i], all_vs[i]), (all_us[i + 1], all_vs[i + 1]), (0,0,255), 2)
+        #     pass    
+
+        col = np.uint8(texture[v, u])
+        col = [int(a) for a in col]
+        col = tuple(col)
+        return (col)

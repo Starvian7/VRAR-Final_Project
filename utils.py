827a613b20ac7e798d30f778425ce1694f65cb3f
NTUT_Starvian <79505152+HuangShengYao108590061@users.noreply.github.com>
2021-12-22 01:26:06 +0800

    Updates

--- utils.py
+++ utils.py
@@ -2,8 +2,6 @@ import numpy as np
 import math
 
 def get_extended_RT(A, H):
-	#finds r3 and appends
-	# A is the intrinsic mat, and H is the homography estimated
 	H = np.float64(H) #for better precision
 	A = np.float64(A)
 	R_12_T = np.linalg.inv(A).dot(H)
@@ -12,8 +10,6 @@ def get_extended_RT(A, H):
 	r2 = np.float64(R_12_T[:, 1]) #col2
 	T = R_12_T[:, 2] #translation
 	
-	#ideally |r1| and |r2| should be same
-	#since there is always some error we take square_root(|r1||r2|) as the normalization factor
 	norm = np.float64(math.sqrt(np.float64(np.linalg.norm(r1)) * np.float64(np.linalg.norm(r2))))
 	
 	r3 = np.cross(r1,r2)/(norm)
